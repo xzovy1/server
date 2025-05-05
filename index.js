6507c9ee0df6026c5176a64579c034b0f1a20788
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-05-05 15:45:20 -0600

    fix: defaults to home page and correctly displays 404 page on error

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 import http from 'node:http';
 import fs from 'node:fs';
-import url from 'url';
-import path from 'path';
 
-const PORT = 5000;
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// console.log(__filename, __dirname);
+const PORT = 8080;
 
 const server = http.createServer((req, res)=>{
-    let fileName = "." + req.url;
     
+    if(req.url == '/'){
+        res.setHeader('Location', '/home.html');
+        req.url = '/home.html';
+    }
+    
+    let fileName = "." + req.url;
+
     fs.readFile(fileName, 'utf-8', (err, data) => {
-        fs.link('/', 'home.html')
-        console.log(req.url)
         if(err){
-            fileName = './404.html' 
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.write('404')
-            return res.end();
+            fs.readFile('./404.html', (err404, data404) => {
+                if(err404){
+                    res.writeHead(404, {'Content-Type': 'text/html'});
+                    return res.end('404 Not Found');
+                }
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end(data404);
+            })
         }
         else{
             res.writeHead(200, {'Content-Type': 'text/html'});
+            
             res.write(data);
             return res.end();
         }
